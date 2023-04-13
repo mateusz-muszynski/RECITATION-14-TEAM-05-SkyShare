fa29f212cd836a3f96a55e1669d65ca8ef79ab58
Jennifer Kim <jeki5853@colorado.edu>
2023-04-13 11:25:14 -0600

    replaced app.listen with module.exports

--- all project code&components/index.js
+++ all project code&components/index.js
@@ -70,7 +70,7 @@ app.get('/welcome', (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
 
 
