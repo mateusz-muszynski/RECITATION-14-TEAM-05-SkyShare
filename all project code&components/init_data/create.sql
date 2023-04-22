45bee6d48cebf25cd20273679bc831002fd5d8d2
Jennifer Kim <jeki5853@colorado.edu>
2023-04-22 04:00:59 -0600

    all tables needed for Jennifer's code of uploading Photos

--- all project code&components/init_data/create.sql
+++ all project code&components/init_data/create.sql
@@ -19,40 +19,20 @@ CREATE TABLE IF NOT EXISTS followers (
 DROP TABLE IF EXISTS photos CASCADE;
 CREATE TABLE IF NOT EXISTS photos (
   photo_id SERIAL PRIMARY KEY NOT NULL,
-  photo_state varchar(250),
+  photo_state varchar(250), /* valid inputs are Colorado, California, Montana, etc */
  /* photo_description VARCHAR(255), */
   photo_url VARCHAR(400),
   user_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(user_id)
-   /*photo_file_path VARCHAR(255) */
 );
 
+
+
+/* WE DO NOT NEED THIS TABLE !!!!
 DROP TABLE IF EXISTS photos_to_users CASCADE; 
 CREATE TABLE IF NOT EXISTS photos_to_users (
   photo_id INT,
   user_id INT,
   FOREIGN KEY (photo_id) REFERENCES photos(photo_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-DROP TABLE IF EXISTS photosfake CASCADE; 
-CREATE TABLE IF NOT EXISTS photosfake (
-  photo_id SERIAL PRIMARY KEY NOT NULL,
-  user_id INT NOT NULL,
-  image_caption VARCHAR(200),
-  location VARCHAR(255),
-  image_url VARCHAR(300) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+); */
