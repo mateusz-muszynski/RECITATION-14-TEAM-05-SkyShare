09f3eea09af574daa0e80a52a1b4e44ed35926bc
Jennifer Kim <jeki5853@colorado.edu>
2023-04-22 04:10:54 -0600

    empty cloudinaryjs file

--- all project code&components/cloudinary.js
+++ all project code&components/cloudinary.js
@@ -1,9 +1,11 @@
 const cloudinary = require('cloudinary');
 
-cloudinary.config({
-    cloud_name: "duelyepmz",
-    api_key: "511679172543115",
-    api_secret: "nUmoqZOXPfC57W0srC0Yx3Mp3WM"
+cloudinary.config({ // JENNIFER     IMPORTANT : SEE DISCORD ABOUT THIS
+    cloud_name: "",
+    api_key: "",
+    api_secret: ""
   });
 
 module.exports = cloudinary;
+
+
